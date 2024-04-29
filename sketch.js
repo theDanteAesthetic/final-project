be243d4b79df245d2697b2789193e708ab427fb7
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-04-29 14:04:26 -0700

    Added sliders for dialation and made a resetVectors() function to make sure the vectors don't grow via the draw() loop

--- sketch.js
+++ sketch.js
@@ -17,12 +17,16 @@ let vectors = [
 
 function draw() {
   background(0);
+  //resetVectors();
   horizon();
   cameraP();
   drawPoints();
   rTransformX();
   rTransformY();
   rTransformZ();
+  dTransformX();
+  dTransformY();
+  dTransformZ();
 }
 
 function horizon() {
@@ -43,17 +47,22 @@ function cameraP(){
   pop();
 }
 
+function resetVectors(){
+  for(let i = 0; i<vectors.length; i++){
+    vectors[i].x = vectors[i].r.x;
+    vectors[i].y = vectors[i].r.y;
+    vectors[i].z = vectors[i].r.z;
+  }
+      
+}
 function rTransformX(){
   //this function rotates around the x axis, not the x direction
   push();
     angleMode(DEGREES);
-    let aa = document.getElementById("x").value;
+    let aa = document.getElementById("xR").value;
+    resetVectors();
     for(let i = 0; i < vectors.length; i++){
       console.log(aa);
-      vectors[i].x = vectors[i].r.x;
-      vectors[i].y = vectors[i].r.y;
-      vectors[i].z = vectors[i].r.z;
-      
       vectors[i].x = vectors[i].x;
       vectors[i].y = vectors[i].y*cos(aa)-vectors[i].z*sin(aa);
       vectors[i].z = vectors[i].y*sin(aa)+vectors[i].z*cos(aa);
@@ -66,11 +75,11 @@ function rTransformY(){
   //this function rotates around the y axis, not the y direction
   push();
     angleMode(DEGREES);
-    let aa = document.getElementById("y").value;
+    let aa = document.getElementById("yR").value;
     for(let i = 0; i < vectors.length; i++){
       vectors[i].x = vectors[i].x*cos(aa) + vectors[i].z*sin(aa);
       vectors[i].y = vectors[i].y;
-      vectors[i].z = -1*vectors[i].x*sin(aa) + vectors[i].z*cos(aa);
+      vectors[i].z = -vectors[i].x*sin(aa) + vectors[i].z*cos(aa);
       //console.log(vectors[i].x, vectors[i].y, vectors[i].z);
     }
   pop();
@@ -80,7 +89,7 @@ function rTransformZ(){
   //this function rotates around the z axis, not the z direction
   push();
     angleMode(DEGREES);
-    let aa = document.getElementById("z").value;
+    let aa = document.getElementById("zR").value;
     for(let i = 0; i < vectors.length; i++){
 
       vectors[i].x = vectors[i].x*cos(aa) - vectors[i].y*sin(aa);
@@ -90,13 +99,43 @@ function rTransformZ(){
     }
   pop();
 }
+
+function dTransformX(){
+  //this function dialates the x axis
+  push();
+    let dd = document.getElementById("xD").value;
+    for(let i = 0; i < vectors.length; i++){
+      vectors[i].x *= dd;
+    }
+  pop();
+}
+
+function dTransformY(){
+  //this function dialates the y axis
+  push();
+    let dd = document.getElementById("yD").value;
+    for(let i = 0; i < vectors.length; i++){
+      vectors[i].y *= dd;
+    }
+  pop();
+}
+
+function dTransformZ(){
+  //this function dialates the z axis
+  push();
+    let dd = document.getElementById("zD").value;
+    for(let i = 0; i < vectors.length; i++){
+      vectors[i].z *= dd;
+    }
+  pop();
+}
 function drawPoints(){
   push();
     translate(width/2, height/2);
     stroke('lime');
     strokeWeight(2);
     for(let i = 0; i < vectors.length; i++){
-      ellipse(vectors[i].x*50, vectors[i].y*50,5,5);
+      //ellipse(vectors[i].x*50, vectors[i].y*50,5,5);
       for(let u = 0; u<vectors.length;u++){
         if(vectors[i]!=vectors[u]){
           if((vectors[i].r.x === vectors[u].r.x || vectors[i].r.y === vectors[u].r.y) && vectors[i].r.z === vectors[u].r.z){
@@ -110,4 +149,3 @@ function drawPoints(){
     }
   pop();
 }
-
