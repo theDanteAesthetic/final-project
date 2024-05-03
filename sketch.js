71b88dac172729402938918badb76e651e06f835
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-05-03 15:01:04 -0700

    Added a fully functional matrix input using 9 number inputs. Might change to text inputs later to interperet trig functions.

--- sketch.js
+++ sketch.js
@@ -18,149 +18,40 @@ let vectors = [
 function draw() {
   background(0);
   //resetVectors();
-  horizon();
-  cameraP();
-  //drawPoints();
-  rTransformX();
-  rTransformY();
-  rTransformZ();
-  dTransformX();
-  dTransformY();
-  dTransformZ();
+  makeAxis();
   makeBox();
 }
 
-function horizon() {
+function makeAxis(){
   push();
-    stroke(255);
-    strokeWeight(2);
-    line(0, height/2, width, height/2);
+    rotateX(30);
+    rotateY(30)
+    noFill();
+    stroke(255)
+    box(width, 0, 0)
+    box(0, height, 0)
+    box(0, 0, width+height)
   pop();
 }
-
-function cameraP(){
-  push();
-    translate(width/2, height/2);
-    stroke('red');
-    strokeWeight(2);
-    line(-5, 5, 5, -5)
-    line(5, 5, -5, -5)
-  pop();
-}
-
-function resetVectors(){
-  for(let i = 0; i<vectors.length; i++){
-    vectors[i].x = vectors[i].r.x;
-    vectors[i].y = vectors[i].r.y;
-    vectors[i].z = vectors[i].r.z;
-  }
-      
-}
-function rTransformX(){
-  //this function rotates around the x axis, not the x direction
-  push();
-    angleMode(DEGREES);
-    let aa = document.getElementById("xR").value;
-    resetVectors();
-    for(let i = 0; i < vectors.length; i++){
-      console.log(aa);
-      vectors[i].x = vectors[i].x;
-      vectors[i].y = vectors[i].y*cos(aa)-vectors[i].z*sin(aa);
-      vectors[i].z = vectors[i].y*sin(aa)+vectors[i].z*cos(aa);
-      //console.log(vectors[i].x, vectors[i].y, vectors[i].z);
-    }
-  pop();
-}
-
-function rTransformY(){
-  //this function rotates around the y axis, not the y direction
-  push();
-    angleMode(DEGREES);
-    let aa = document.getElementById("yR").value;
-    for(let i = 0; i < vectors.length; i++){
-      vectors[i].x = vectors[i].x*cos(aa) + vectors[i].z*sin(aa);
-      vectors[i].y = vectors[i].y;
-      vectors[i].z = -vectors[i].x*sin(aa) + vectors[i].z*cos(aa);
-      //console.log(vectors[i].x, vectors[i].y, vectors[i].z);
-    }
-  pop();
-}
-
-function rTransformZ(){
-  //this function rotates around the z axis, not the z direction
-  push();
-    angleMode(DEGREES);
-    let aa = document.getElementById("zR").value;
-    for(let i = 0; i < vectors.length; i++){
-
-      vectors[i].x = vectors[i].x*cos(aa) - vectors[i].y*sin(aa);
-      vectors[i].y = vectors[i].x*sin(aa) + vectors[i].y*cos(aa);
-      vectors[i].z = vectors[i].z;
-      //console.log(vectors[i].x, vectors[i].y, vectors[i].z);
-    }
-  pop();
-}
-
-function dTransformX(){
-  //this function dialates the x axis
-  push();
-    let dd = document.getElementById("xD").value;
-    for(let i = 0; i < vectors.length; i++){
-      vectors[i].x *= dd;
-    }
-  pop();
-}
-
-function dTransformY(){
-  //this function dialates the y axis
-  push();
-    let dd = document.getElementById("yD").value;
-    for(let i = 0; i < vectors.length; i++){
-      vectors[i].y *= dd;
-    }
-  pop();
-}
-
-function dTransformZ(){
-  //this function dialates the z axis
-  push();
-    let dd = document.getElementById("zD").value;
-    for(let i = 0; i < vectors.length; i++){
-      vectors[i].z *= dd;
-    }
-  pop();
-}
-function drawPoints(){
-  push();
-    translate(0,0);
-    stroke('lime');
-    strokeWeight(2);
-    for(let i = 0; i < vectors.length; i++){
-      //ellipse(vectors[i].x*50, vectors[i].y*50,5,5);
-      for(let u = 0; u<vectors.length;u++){
-        if(vectors[i]!=vectors[u]){
-          if((vectors[i].r.x === vectors[u].r.x || vectors[i].r.y === vectors[u].r.y) && vectors[i].r.z === vectors[u].r.z){
-            line(vectors[i].x*50,vectors[i].y*50,vectors[u].x*50,vectors[u].y*50);
-            //line(vectors[i].z*50,vectors[i].y*50,vectors[u].z*50,vectors[u].y*50);
-          }
-          if(vectors[i].r.x === vectors[u].r.x && vectors[i].r.y === vectors[u].r.y && vectors[i].r.z != vectors[u].r.z){
-            line(vectors[i].x*50,vectors[i].y*50,vectors[u].x*50,vectors[u].y*50);
-            //line(vectors[i].z*50,vectors[i].y*50,vectors[u].z*50,vectors[u].y*50)
-          }
-        }
-      }
-    }
-  pop();
-}
-
 function makeBox(){
   push();
-    let aa = frameCount * 0.1;
-    applyMatrix(cos(aa), 0, sin(aa), 0, 0, 1, 0, 0, -1*sin(aa), 0, cos(aa), 0, 0, 0, 0, 1);
-    stroke('lime');
+    angleMode(DEGREES);
+    let aa = frameCount * 0.01;
+    let inputMatirx = {
+      m11: document.querySelector("#m11").value,
+      m12: document.querySelector("#m12").value,
+      m13: document.querySelector("#m13").value,
+      m21: document.querySelector("#m21").value,
+      m22: document.querySelector("#m22").value,
+      m23: document.querySelector("#m23").value,
+      m31: document.querySelector("#m31").value,
+      m32: document.querySelector("#m32").value,
+      m33: document.querySelector("#m33").value,
+    }
     strokeWeight(1);
     noFill();
-    rotateX(-aa/2);
+    stroke('lime');
+    applyMatrix(inputMatirx.m11, inputMatirx.m12, inputMatirx.m13, 0, inputMatirx.m21, inputMatirx.m22, inputMatirx.m23, 0, inputMatirx.m31, inputMatirx.m32, inputMatirx.m33, 0, 0, 0, 0, 1);
     box(100, 100, 100);
   pop();
 }
