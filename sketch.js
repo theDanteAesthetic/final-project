e5bb8e489701a1a9f1fa3ee77176dc2f0bbd2329
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-05-07 15:27:58 -0700

    used the beginGeometry() and endGeometry() functions to allow for more detail on the cube render.
    Currently there are details on each side of the cube to help visually see the transformation.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let decBox;
+
 function setup() {
   createCanvas(600, 600, WEBGL);
+  angleMode(DEGREES);
   let defaultCam = createCamera();
-  defaultCam.camera(0, 0, 200, 0, 0, 0, 0, 1, 0)
+  defaultCam.camera(0, 0, 200, 0, 0, 0, 0, 1, 0);
+  makeDecBox();
 }
 
 let vectors = [
@@ -27,15 +31,67 @@ function draw() {
 function makeAxis(){
   push();
     noFill();
-    stroke(255)
+    stroke('#00FF2F')
     box(width, 0, 0)
+    stroke('#0078FF')
     box(0, width, 0)
+    stroke('#FF001F')
     box(0, 0, width)
+  pop();
+}
+
+function makeDecBox(){
+  push();
+    beginGeometry();
+    fill('rgba(255, 255, 255, 0)');
+    box(30, 30, 30);
+    push();
+      translate(0,15,0);
+      fill('#0078FF');
+      noStroke();
+      cylinder(2,0.5);
+    pop();
+    push();
+      translate(0,-15,0);
+      fill('#0078FF');
+      noStroke();
+      cylinder(2,0.5);
+    pop();
+    push();
+      translate(15,0,0);
+      rotateZ(90)
+      fill('#00FF2F');
+      noStroke();
+      cylinder(2,0.5);
+    pop();
+    push();
+    translate(-15,0,0);
+    rotateZ(90)
+      fill('#00FF2F');
+      noStroke();
+      cylinder(2,0.5);
+    pop();
+    push();
+      translate(0,0,15);
+      rotateX(90);
+      fill('#FF001F');
+      noStroke();
+      cylinder(2,0.5);
+    pop();
+    push();
+    translate(0,0,-15);
+    rotateX(90)
+      fill('#FF001F');
+      noStroke();
+      cylinder(2,0.5);
+    pop();
+
+    decBox = endGeometry();
   pop();
 }
 function makeBox(){
   push();
-    angleMode(DEGREES);
+    //angleMode(DEGREES);
     let aa = frameCount * 0.01;
     let inputMatirx = {
       m11: document.querySelector("#m11").value,
@@ -50,8 +106,9 @@ function makeBox(){
     }
     strokeWeight(1);
     fill('rgba(255, 255, 255, 0)');
-    stroke('lime');
+    stroke(255);
     applyMatrix(inputMatirx.m11, inputMatirx.m12, inputMatirx.m13, 0, inputMatirx.m21, inputMatirx.m22, inputMatirx.m23, 0, inputMatirx.m31, inputMatirx.m32, inputMatirx.m33, 0, 0, 0, 0, 1);
-    box(30, 30, 30);
+    model(decBox);
+    //box(30, 30, 30);
   pop();
 }
