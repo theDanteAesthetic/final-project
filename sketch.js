9021daede4b5403cde28f77b48d7fdc57ed7c0f6
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-05-17 15:27:42 -0700

    Changed reset button to run smoother, worked on css

--- sketch.js
+++ sketch.js
@@ -8,17 +8,6 @@ function setup() {
   makeDecBox();
 }
 
-let vectors = [
-  {x: 1, y: 1, z: 1, r:{x:1, y: 1, z:1}},
-  {x: -1, y: 1, z: 1, r:{x:-1, y: 1, z:1}},
-  {x: -1, y: -1, z: 1, r:{x:-1, y: -1, z:1}},
-  {x: 1, y: -1, z: 1, r:{x:1, y: -1, z:1}},
-  {x: 1, y: 1, z: -1, r:{x:1, y: 1, z: -1}},
-  {x: -1, y: 1, z: -1, r:{x: -1, y: 1, z: -1}},
-  {x: -1, y: -1, z: -1, r:{x: -1, y: -1, z: -1}},
-  {x: 1, y: -1, z: -1, r:{x:1, y: -1, z: -1}}
-]
-
 
 function draw() {
   background(0);
@@ -26,6 +15,7 @@ function draw() {
   orbitControl();
   makeAxis();
   makeBox();
+  //updateRV();
 }
 
 function getRotationX(){
@@ -123,12 +113,40 @@ function makeBox(){
     fill('rgba(255, 255, 255, 0)');
     stroke(255);
     applyMatrix(inputMatirx.m11, inputMatirx.m12, inputMatirx.m13, 0, inputMatirx.m21, inputMatirx.m22, inputMatirx.m23, 0, inputMatirx.m31, inputMatirx.m32, inputMatirx.m33, 0, 0, 0, 0, 1);
-    rotateX(xx);
-    rotateY(yy);
-    rotateZ(zz);
+    updateRV();
+    //rotateX(xx);
+    //rotateY(yy);
+    //rotateZ(zz);
     
     model(decBox);
     //box(30, 30, 30);
+  pop();
+}
+
+function updateRV(){
+  push();
+    let xx = document.querySelector("#rX").value;
+    let yy = document.querySelector("#rY").value;
+    let zz = document.querySelector("#rZ").value;
+    document.querySelector("#rX").addEventListener("input", () => {
+      document.querySelector("#m22").value = (cos(xx));
+      document.querySelector("#m23").value = (-1*sin(xx));
+      document.querySelector("#m32").value = (sin(xx));
+      document.querySelector("#m33").value = (cos(xx));
+    })
+    document.querySelector("#rY").addEventListener("input", () => {
+      document.querySelector("#m11").value = (cos(yy));
+      document.querySelector("#m13").value = (sin(yy));
+      document.querySelector("#m31").value = (-1*sin(yy));
+      document.querySelector("#m33").value = (cos(yy));
+    })
+    document.querySelector("#rZ").addEventListener("input", () => {
+      document.querySelector("#m11").value = (cos(zz));
+      document.querySelector("#m12").value = (-1*sin(zz));
+      document.querySelector("#m21").value = (sin(zz));
+      document.querySelector("#m22").value = (cos(zz));
+    })
+      
   pop();
 }
 
@@ -136,7 +154,19 @@ function reset(){
   push();
     let resetButton = document.querySelector("#resetButton");
     resetButton.addEventListener("click", () => {
-      window.location.reload();
+      document.querySelector("#m11").value = 1;
+      document.querySelector("#m12").value = 0;
+      document.querySelector("#m13").value = 0;
+      document.querySelector("#m21").value = 0;
+      document.querySelector("#m22").value = 1;
+      document.querySelector("#m23").value = 0;
+      document.querySelector("#m31").value = 0;
+      document.querySelector("#m32").value = 0;
+      document.querySelector("#m33").value = 1;
+
+      document.querySelector("#rX").value = 0;
+      document.querySelector("#rY").value = 0;
+      document.querySelector("#rZ").value = 0;
     })
   pop();
 }
